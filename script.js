909c44050e7634d6460dd00351ac287e06b0a471
benjibennett12 <benjaminbennett0412@gmail.com>
2023-11-02 16:54:39 -0400

    Responsive Changes

--- script.js
+++ script.js
@@ -7,19 +7,18 @@ const nameError = document.getElementById('nameError');
 const phoneError = document.getElementById('phoneError');
 const emailError = document.getElementById('emailError');
 const dropdownError = document.getElementById('dropdownError');
-const thankyouModal = document.getElementById('thankyou');
+const thankyouModal = document.querySelector('.modal');
 const modalText = document.querySelector('.modal-text');
 const backButton = document.getElementById('back');
 
+// subscribeForm.addEventListener("click", handleSubscribe);
 
-function handleSubscribe() {
-    console.log("handlesubscibe");
-    e.preventDefault();
+function handleSubscribe(event) {
+    event.preventDefault();
     validateName(name);
     validatePhone(phoneNumber);
     validateEmail(email);
-
-
+    showThankYouModal();
 }
 
 
@@ -39,27 +38,33 @@ function validateName(name) {
 
 function validatePhone(phoneNumber) {
     const phoneValue = phoneNumber.value.trim();
+    const phoneRegex = /^\d+$/;
 
     if (phoneValue === '' || phoneValue === undefined) {
         phoneError.textContent = 'Please fill out this form';
         phoneError.style.display = 'block';
         phone.style.border = '2px solid red';
-    } else if (!Number.isInteger(Number(phoneValue))) {
+    } else if (!phoneRegex.test(phoneValue)) {
         phoneError.textContent = 'Numbers Only';
         phoneError.style.display = 'block';
         phone.style.border = '2px solid red';
-
     } else {
         phoneError.style.display = 'none';
         phone.style.border = '2px solid #09c372';
     }
 }
 
+
 function validateEmail(email) {
     const emailValue = email.value.trim();
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
 
     if (emailValue === '' || emailValue === undefined) {
         emailError.textContent = 'Please fill out this form';
+        emailError.style.display = 'block';
+        email.style.border = '2px solid red';
+    } else if (!emailRegex.test(emailValue)) {
+        emailError.textContent = 'Invalid email address';
         emailError.style.display = 'block';
         email.style.border = '2px solid red';
     } else {
@@ -68,10 +73,16 @@ function validateEmail(email) {
     }
 }
 
+
 function showThankYouModal() {
-    thankyouModal.style.display = 'block';
-    modalText.textContent = 'Thank you for your submission!';
+    if (nameError.style.display === 'none' && phoneError.style.display === 'none' && emailError.style.display === 'none') {
+        return thankyouModal.style.display = 'block';
+    } else {
+        return thankyouModal.style.display = 'none';
+    }
 }
+
+
 
 backButton.addEventListener('click', function () {
     thankyouModal.style.display = 'none';
